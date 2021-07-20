d88fbbb6132f63f037fa7af2e125e33ceacdd58b
OnlyDropped <asheburenkov@yandex.ru>
2021-07-20 13:48:34 +0300

    added slider

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ $(function () {
 
   $('.select-style').styler();
 
+
+  $('.slider').slick({
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
 });
