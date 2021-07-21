f402eee0067f5bffc7bc2d629b032b40b0b31637
OnlyDropped <asheburenkov@yandex.ru>
2021-07-21 12:39:04 +0300

    added arrow slider

--- js/main.js
+++ js/main.js
@@ -5,10 +5,14 @@ $(function () {
 
   $('.slider').slick({
     dots: false,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-next.svg" alt=""></button>',
     autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 3,
     slidesToScroll: 3,
+
     responsive: [
       {
         breakpoint: 360,
